2a36d5f4e28e69b9ac754df4218c02b1def1cfad
Alexandre Albuquerque <albuq.alexandre@gmail.com>
2017-06-21 18:35:40 -0300

    Initial Commit

---
+++ GBens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GBens
+//
+//  Created by Alexandre de Sousa Albuquerque on 21/06/17.
+//  Copyright © 2017 Alexandre de Sousa Albuquerque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
